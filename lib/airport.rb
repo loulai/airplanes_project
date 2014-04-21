f05cea6d4fe38398c45ff77ecfd0a6cacb3dffc7
Louise <louiselai88@gmail.com>
2014-04-21 14:57:48 +0100

    the basics of the airport is complete! Hurrah! All tests pass.

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,23 +1,44 @@
 class Airport
 
-	def initialize(something = [])
-		@airport_bay = something
+	MAX_CAPACITY	= 6
+
+	def initialize(capacity = MAX_CAPACITY)
+		@capacity    = capacity
+		@airport_bay = []
+	end
+
+	def capacity
+		@capacity
+	end
+
+	def capacity=(new_value)
+		@capacity = new_value
+	end
+
+	def full?
+		planes_count == MAX_CAPACITY
 	end
 
 	def park!(plane)
+		raise "Airport is full" if full?
 		@airport_bay << plane
-		@airport_bay
 	end
 
 	def has_plane?
 		@airport_bay.any?
 	end
 
-	def take_off!(n)
-		@airport_bay.drop(n)
+	def take_off!(plane)
+		planes.delete(plane)
 	end
 
+	def planes
+		@airport_bay
+	end
 
+	def planes_count
+		@airport_bay.count
+	end
 
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -16,25 +16,37 @@ describe "Airport" do
 
 		it 'has one plane after one plane has parked' do
 			fake_plane = double :plane
-			expect(airport.park!(fake_plane).count).to eq 1
+			airport.park!(fake_plane)
+			expect(airport.planes_count).to eq 1
+		end
+
+		it 'has zero planes after one plane has taken off' do
+			fake_plane = double :plane
+			airport.park!(fake_plane)
+			airport.take_off!(fake_plane)
+			expect(airport.planes_count).to eq 0
+		end
+
+	context 'traffic control' do
+
+			it "knows when it's full (the max capacity has been reached)" do
+				airport.capacity.times { airport.park!(:plane) }
+				expect(airport).to be_full
+			end
+		
+			it 'a plane cannot land if the airport is full' do
+				airport.capacity.times { airport.park!(:plane) }
+				expect(lambda {airport.park!(:plane)}).to raise_error(RuntimeError)
+			end
+
+	end
+
+
 		# my_plane = double :plane
 		# my_airport = Airport.new
 		# expect(my_airport).to be_empty
 		# my_airport.grab(my_plane)
 		# expect(my_airport.planes_count).to eq 1
-		end
-
-		it 'has one less plane after one plane has taken off' do
-			fake_plane = double :plane
-			airport_with_one_plane = Airport.new([fake_plane])
-			expect(airport_with_one_plane.take_off!(1).count).to eq 0
-		end
-
-end
-
-
-
-
 
 		# it 'has two planes after another plane has parked' do
 		# 	fake_plane = double :plane
